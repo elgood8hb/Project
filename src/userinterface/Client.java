51656a5f8d3e80f3b4f8b10ba125ee1e62b5cd42
gkemp <gkemp@130.203.84.203>
2019-02-19 18:06:25 -0500

    test

---
+++ src/userinterface/Client.java
@@ -0,0 +1,35 @@
+
+package userinterface;
+
+
+import java.net.*;
+import java.io.*;
+
+public class Client extends ClientServer {
+
+     protected Socket socket;
+     private int newPort = 11000;
+}
+
+public Client(String url, int port) {
+         try {
+             
+             
+         } catch (Exception e) {
+             
+         }
+     } //Client()
+
+public void run() {
+         try {
+             
+         } catch (IOException e) { 
+            
+         }
+     } // run()
+
+public static void main(String args[]) {
+  
+
+} // main()
+
---
+++ src/userinterface/MasterServer.java
@@ -0,0 +1,56 @@
+
+package userinterface;
+
+
+package ClientServer;
+
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author gkemp
+ */
+
+
+public class MasterServer extends ClientServer {
+    
+    private int clientPortNumber;
+    private ServerSocket port;
+    private Socket socket;
+    //private DatabaseServer ThreadedServer[];
+    private int numberServer;
+    private String stNum;
+    
+        
+    public MasterServer(int portNum, int nBacklog)  {
+        
+        clientPortNumber = socket.getPort();
+        
+        try {
+            
+        } catch (IOException e) {
+            
+        }     
+    } 
+ 
+    
+    public void run() {
+        try {
+            
+            while(true) {
+                
+                  
+                
+            }
+        } catch (IOException e) {
+            
+        }
+    } // run()
+
+
+     
+    public static void main(String args[]) {
+        
+    } // main()
+} // MasterServer
+
